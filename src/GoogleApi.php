cbadffbfe4df5313b0aae0baa071d1510945bb91
Igor A Tarasov <develop@dicr.org>
2019-09-09 20:06:10 +0500

    авторизация через отдельный конфиг authConfig, позволяющий указать путь файла

--- src/GoogleApi.php
+++ src/GoogleApi.php
@@ -2,16 +2,14 @@
 namespace dicr\google;
 
 use yii\base\Component;
-use yii\helpers\ArrayHelper;
 
 /**
  * Компонент для настройки и создания клиента Google API.
  *
  * @property-read \Google_Client $client клиент Google API
- * @property array $sessionToken токен доступа в сессии пользователя.
  *
- * @link https://developers.google.com/identity платформа Google Identity
- * @link https://github.com/googleapis/google-api-php-client PHP-клиент
+ * @link https://github.com/googleapis/google-api-php-client PHP-клиент с примерами и документацией
+ * @link https://cloud.google.com/docs/authentication документация по авторизации Google
  * @link https://console.developers.google.com регистрация приложения и управление ключами доступа
  *
  * @author Igor (Dicr) Tarasov <develop@dicr.org>
@@ -21,10 +19,8 @@ class GoogleApi extends Component
 {
     /**
      * @var array конфиг по-умолчанию для создания клиента.
-     * Парамеры инициализации \Google_Client.
      *
-     * Дополнительно добавлен отдельно параметр 'scopes', кторый при создании клиента
-     * инициализируется отдельно от конфига.
+     * Парамеры инициализации \Google_Client.
      *
      * Пример:
      * [
@@ -32,17 +28,53 @@ class GoogleApi extends Component
      *  'client_secret' => 'XXXXXXXXXXXX',
      *  'access_type' => 'offline',
      *  'prompt' => 'select_account consent',
-     *  'include_granted_scopes' => true,
-     *  'scopes' => [
-     *      \Google_Service_Sheets::SPREADSHEETS,
-     *      \Google_Service_Sheets::DRIVE,
-     *      \Google_Service_Sheets::DRIVE_FILE
-     *  ]
+     *  'include_granted_scopes' => true
+     * ]
      *
-     *  @see \Google_Client
+     *  @see \Google_Client::__construct()
      *  @link https://github.com/googleapis/google-api-php-client
      */
     public $clientConfig = [];
+
+    /**
+     * @var string|array данные авторизации
+     * Может быть путь к файлу auth.json, скачанному с Google Developer Console при создании приложения,
+     * либо массив парамеров из файла json
+     *
+     * Прмер:
+     *  @app/config/auth.json
+     *
+     *  либо:
+     *  [
+     *    'type' => 'service_account',
+     *    'project_id' => 'XXXXX',
+     *    'private_key_id': 'XXXXX',
+     *    'private_key': 'XXXXXXX',
+     *    'client_email': 'xxx@xxx.iam.gserviceaccount.com,
+     *    'client_id': 'XXXXXX',
+     *    'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
+     *    'token_uri': "https://oauth2.googleapis.com/token',
+     *    'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
+     *    'client_x509_cert_url': "https://www.googleapis.com/robot/v1/metadata/x509/xxx%40xxx.gserviceaccount.com"
+     * ]
+     *
+     * @see \Google_Client::setAuthConfig($config)
+     */
+    public $authConfig;
+
+    /**
+     * @var string[] запрашиваемые разрешения приложения.
+     *
+     * Пример:
+     * [
+     *      \Google_Service_Sheets::SPREADSHEETS,
+     *      \Google_Service_Sheets::DRIVE,
+     *      \Google_Service_Sheets::DRIVE_FILE
+     * ]
+     *
+     * @see \Google_client::setScopes($scopes)
+     */
+    public $scopes = [];
 
     /**
      * {@inheritDoc}
@@ -50,12 +82,76 @@ class GoogleApi extends Component
      */
     public function init()
     {
+        // client config
         $this->clientConfig = (array)($this->clientConfig ?: []);
 
         // default Application Name
         if (!isset($this->clientConfig['application_name'])) {
             $this->clientConfig['application_name'] = \Yii::$app->name;
         }
+
+        // разворачиваем алиас в authConfig
+        if (is_string($this->authConfig)) {
+            $this->authConfig = \Yii::getAlias($this->authConfig, true);
+
+            // устанавливаем переменные окружения для сервисных аккаунтов
+            // @link https://github.com/googleapis/google-api-php-client/blob/master/docs/oauth-server.md
+            putenv('GOOGLE_APPLICATION_CREDENTIALS=' . $this->authConfig);
+        }
+
+        $this->scopes = (array)($this->scopes ?: []);
+    }
+
+    /**
+     * Создает и настраивает клиента API Google.
+     *
+     * @param array $config дополнительные парамеры клиента, перезаписывающие парамеры конфига по-умлчанию.
+     * Конфиг клиента это парамеры объекта \Google_Client, а также параметр scopes
+     *
+     * @return \Google_Client
+     */
+    public function getClient(array $config = [])
+    {
+        // объединяем с парамерами по-умолчанию
+        if (!empty($this->clientConfig)) {
+            $config = array_merge($this->clientConfig, $config);
+        }
+
+        // создаем клиента
+        $client = new \Google_Client($config);
+
+        // устанавливаем параметры авторизации
+        if (!empty($this->authConfig)) {
+            $client->setAuthConfig($this->authConfig);
+        }
+
+        // устанавливаем scopes
+        if (!empty($this->scopes)) {
+            $client->setScopes($this->scopes);
+        }
+
+        // получаем токен из сессии пользователя
+        $token = static::getSessionToken();
+        if (!empty($token)) {
+            // устанавливаем токен доступа
+            $client->setAccessToken($token);
+
+            // если срок токена истек
+            if ($client->isAccessTokenExpired()) {
+                // получаем токен обновления
+                $refreshToken = $client->getRefreshToken();
+                if (!empty($refreshToken)) {
+                    // пытаемся получить новый токен
+                    $token = $client->fetchAccessTokenWithRefreshToken($refreshToken);
+                    // сохраняем новый в сессии пользователя
+                    if (!empty($token)) {
+                        static::setSessionToken($token);
+                    }
+                }
+            }
+        }
+
+        return $client;
     }
 
     /**
@@ -71,9 +167,9 @@ class GoogleApi extends Component
      *    'refresh_token' => 'XXXXXXXX'
      * ]
      */
-    public function getSessionToken()
+    public static function getSessionToken()
     {
-        $data = \Yii::$app->session->get(__CLASS__, []);
+        $data = \Yii::$app->session->get(static::class, []);
         $token = $data['token'] ?? null;
 
         // если в токене ошибка, то удаляем его
@@ -89,7 +185,7 @@ class GoogleApi extends Component
      *
      * @param array $token данные токена, полученные от Google (набор данных)
      */
-    public function setSessionToken(array $token = null)
+    public static function setSessionToken(array $token = null)
     {
         // сбрасываем некорректный токен
         if (!empty($token) && !empty($token['error'])) {
@@ -97,61 +193,13 @@ class GoogleApi extends Component
         }
 
         // получаем данные сессии
-        $data = \Yii::$app->session->get(__CLASS__, []);
+        $data = \Yii::$app->session->get(static::class, []);
         if (!empty($token)) {
             $data['token'] = $token;
         } else {
             unset($data['token']);
         }
 
-        \Yii::$app->session->set(__CLASS__, $data);
-    }
-
-    /**
-     * Создает и настраивает клиента API Google.
-     *
-     * @param array $config дополнительные парамеры клиента, перезаписывающие парамеры конфига по-умлчанию.
-     * Конфиг клиента это парамеры объекта \Google_Client, а также параметр scopes
-     *
-     * @return \Google_Client
-     */
-    public function getClient(array $config = [])
-    {
-        // получаем конечный вариант конфига
-        $config = array_merge($this->clientConfig, $config);
-
-        // выделяем scopes из конфига
-        $scopes = ArrayHelper::remove($config, 'scopes');
-
-        // создаем клиент
-        $client = new \Google_Client($config);
-
-        // устанавливаем scopes
-        if (!empty($scopes)) {
-            $client->setScopes($scopes);
-        }
-
-        // получаем токен из сессии пользователя
-        $token = $this->getSessionToken();
-        if (!empty($token)) {
-            // устанавливаем токен доступа
-            $client->setAccessToken($token);
-
-            // если срок токена истек
-            if ($client->isAccessTokenExpired()) {
-                // получаем токен обновления
-                $refreshToken = $client->getRefreshToken();
-                if (!empty($refreshToken)) {
-                    // пытаемся получить новый токен
-                    $token = $client->fetchAccessTokenWithRefreshToken($refreshToken);
-                    // сохраняем новый в сессии пользователя
-                    if (!empty($token)) {
-                        $this->setSessionToken($token);
-                    }
-                }
-            }
-        }
-
-        return $client;
+        \Yii::$app->session->set(static::class, $data);
     }
 }
